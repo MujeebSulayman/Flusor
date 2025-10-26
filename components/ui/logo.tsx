0e13f03b8ad9f56af716dfe572ccf19858da28c7
MujeebSulayman <sulaymanmujeeb6@gmail.com>
2025-10-26 13:56:13 +0100

    Enhance Logo component: increase image size and add padding

--- components/ui/logo.tsx
+++ components/ui/logo.tsx
@@ -2,13 +2,13 @@ import Image from 'next/image';
 
 export default function Logo() {
   return (
-    <div className="flex items-center">
+    <div className="flex items-center p-2">
       <Image
         src="/logo.png"
         alt="Flusor Logo"
-        width={32}
-        height={32}
-        className="rounded-lg"
+        width={48}
+        height={48}
+        className="rounded-lg object-contain"
       />
     </div>
   );
